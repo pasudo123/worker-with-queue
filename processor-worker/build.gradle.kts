584822875424b5755c9766308ff5ae52307ed0f5
PARK SUNG DONG <pasudo123@naver.com>
2022-01-15 17:54:17 +0900

    feat : quartz

--- processor-worker/build.gradle.kts
+++ processor-worker/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     runtimeOnly("mysql:mysql-connector-java")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
